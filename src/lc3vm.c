3ca6e150599729b988d914497340978cb0087e8a
Tony Snowden <tsnowden@leomail.tamuc.edu>
2026-04-27 01:31:39 +0000

    Fix Task 4 trap and rti

--- src/lc3vm.c
+++ src/lc3vm.c
@@ -495,7 +495,22 @@ void trap(uint16_t i)
 
   reg[RPC] = mem_read(TRP(i));
 }
+void rti(uint16_t i)
+{
+  (void)i;
 
+  reg[PSR] = mem_read(reg[R6]);
+  pop();
+
+  reg[RPC] = mem_read(reg[R6]);
+  pop();
+
+  if (is_user_mode())
+  {
+    reg[SSP] = reg[R6];
+    reg[R6] = reg[USP];
+  }
+}
 /** @brief reserved
  *
  * Reserved/unused opcode (1101).  We generate an illegal opcode
@@ -520,22 +535,6 @@ void res(uint16_t i) {}
  *   executing.  The low 7 bits i[7:0] contain the trap service vector
  *   index to be invoked.
  */
-void trap(uint16_t i)
-{
-  uint16_t temp = reg[PSR];
-
-  if (is_user_mode())
-  {
-    reg[USP] = reg[R6];
-    reg[R6] = reg[SSP];
-    supervisor_mode();
-  }
-
-  push(reg[RPC]);
-  push(temp);
-
-  reg[RPC] = mem_read(TRP(i));
-}
 
 /**
  * LC-3 instruction microcode store / lookup table.  Need to define array
